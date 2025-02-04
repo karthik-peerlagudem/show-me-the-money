ae3d78ddde6951746c1b8b94eeae67305d7b8ab3
karthik-peerlagudem <karthik-peerlagudem@users.noreply.github.com>
2025-02-05 05:43:50 +1100

    bug fix: cors, eslint issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,15 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 import router from './router';
 
 const app = express();
 
 app.use(express.json());
-
-// enabling CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+app.use(cors());
 
 app.use('/api', router);
 
